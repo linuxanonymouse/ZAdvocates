fd816c6aed4135cba428e3045640837d9cf6d59a
linuxanonymouse <linuxanonymouse@gmail.com>
2026-07-21 13:15:51 +0300

    fix: bind backend to 0.0.0.0 to fix docker networking

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS for Next.js
-  await app.listen(process.env.PORT ?? 4001); // Backend running on 4001
+  await app.listen(process.env.PORT ?? 4001, '0.0.0.0'); // Backend running on 4001
 }
 bootstrap();
